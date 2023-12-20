0054e4e84cb53cf2ea777783163c177512c816b8
Lionel <joechipjoechip@gmail.com>
2023-12-20 13:29:43 +0100

    test nano store : ok

--- src/components/vue/Hello.vue
+++ src/components/vue/Hello.vue
@@ -1,28 +1,46 @@
 <script setup>
 import { ref } from "vue"
+
+import { globalStore, setName } from "../../stores/globalStore.js"
+import { useStore } from '@nanostores/vue';
+
+const $store = useStore(globalStore);
+
+
 const message = ref("message de base")
 
-function handleClick(){
+function handleClickTest(){
     message.value = "un autre message qui prouve l'interactivité"
+}
+
+function handleClickStoreTest(){
+    setName("Billy")
 }
 
 </script>
 
 <template>
-        <div class="hello-wrapper">
-            <p>
-                ceci est le composant vue Hello.vue
-            </p>
-            <p class="clickable" @click="handleClick">
-                {{ message }}
-            </p>
-        </div>
+    <div class="hello-wrapper">
+        <p>
+            ceci est le composant vue Hello.vue
+        </p>
+        <p class="clickable" @click="handleClickTest">
+            {{ message }}
+        </p>
+        <p @click="handleClickStoreTest">
+            {{ $store.name }}
+        </p>
+    </div>
 </template>
 
 <style lang="scss" scoped>
     .hello {
         &-wrapper {
             border: solid 5px orange;
+
+            p {
+                border: solid 1px green;
+            }
         }
     }
 
