6a913f4bab02b44280b9043c02c78aad7cbc22d6
Lionel <joechipjoechip@gmail.com>
2023-12-20 13:29:36 +0100

    test nano store : ok

--- src/stores/globalStore.js
+++ src/stores/globalStore.js
@@ -1,102 +1,75 @@
-import { defineStore } from 'pinia'
+import { map } from "nanostores"
 
-export const useUserStore = defineStore('storedUser',
-	{
+export const globalStore = map({
+	name: "Paul",
+	currentStepIndex: 2,
+	stepsCount: 0,
+	navigation: {
+		stepGrabed: false,
+		navbar: {
+			isMoving: false,
+			position: { x:0, y:0 }
+		}, 
+		isCurrentlyManipulatedIndex: null
+	},
+	sound: {
+		enabled: true
+	},
+	currentSearch: ""
+})
 
-		state: () => ({
-			name: "Paul",
-			currentStepIndex: 2,
-			bgCurrentIndex: 0,
-			stepsCount: 0,
-			navigation: {
-				stepGrabed: false,
-				navbar: {
-					isMoving: false,
-					position: { x:0, y:0 }
-				}, 
-				isCurrentlyManipulatedIndex: null
-			},
-			bgHomeCount: 95,
-			sound: {
-				enabled: true
-			},
-			currentSearch: ""
-		}),
-
-		actions: {
-			setName( newName ){
-				this.name = newName
-			},
-
-			setCurrentStepIndex(newIndex){
-				if( newIndex <= this.stepsCount - 1 ){
-					this.currentStepIndex = newIndex
-				}
-			},
-
-			setCurrentStepIndexIncrement(){
-				if( this.currentStepIndex < this.stepsCount - 1 ){
-					this.currentStepIndex++
-				}
-			},
-
-			setCurrentStepIndexDecrement(){
-				if( this.currentStepIndex > 0 ){
-					this.currentStepIndex--
-				}
-			},
-
-			setStepsCount( size ){
-				this.stepsCount = size
-			},
-
-			// BACKGROUND
-			setBgCurrentIndex(newIndex){
-				if( newIndex <= 4 ){
-					this.bgCurrentIndex = newIndex
-				} else {
-					this.bgCurrentIndex = 0
-				}
-			},
-			setBgCurrentIndexIncrement(){
-				if( this.bgCurrentIndex < this.bgHomeCount ){
-					this.bgCurrentIndex++
-				}
-			},
-
-			setBgCurrentIndexDecrement(){
-				if( this.bgCurrentIndex > 0 ){
-					this.bgCurrentIndex--
-				}
-			},
+export function setName( newName ){
+	globalStore.setKey("name", newName)
+}
 
 
-			// NAVIGATION
-			setstepGrabed( bool ){
-				this.navigation.stepGrabed = bool
-			},
-
-			setIsCurrentlyManipulatedIndex( payload ){
-				this.navigation.isCurrentlyManipulatedIndex = payload
-			},
-
-			setNavigationNavbarPosition( obj ){
-				this.navigation.navbar.position = obj
-			},
-
-			setNavigationNavbarIsMoving( bool ){
-				this.navigation.navbar.isMoving = bool
-			},
-
-			// SEARCH
-			setCurrentSearch( payload ){
-				this.currentSearch = payload
-			}
-		},
-
-		// @TODO : restablish at true later
-		persist: false
-
+// STEPS
+export function setCurrentStepIndex(newIndex){
+	if( newIndex <= globalStore.get().stepsCount - 1 ){
+		globalStore.setKey("currentStepIndex", newIndex)
 	}
+}
 
-)
+export function setCurrentStepIndexIncrement(){
+	const currentStepIndex = globalStore.get().currentStepIndex
+
+	if( currentStepIndex < globalStore.get().stepsCount - 1 ){
+		globalStore.setKey("currentStepIndex", currentStepIndex + 1)
+	}
+}
+
+export function setCurrentStepIndexDecrement(){
+	const currentStepIndex = globalStore.get().currentStepIndex
+	
+	if( currentStepIndex > 0 ){
+		globalStore.setKey("currentStepIndex", currentStepIndex - 1)
+	}
+}
+
+export function setStepsCount( size ){
+	globalStore.setKey("stepsCount", size)
+}
+
+
+// NAVIGATION
+export function setstepGrabed( bool ){
+	globalStore.setKey("navigation", { stepGrabed: bool })
+}
+
+export function setIsCurrentlyManipulatedIndex( payload ){
+	globalStore.setKey("navigation", { isCurrentlyManipulatedIndex: payload })
+}
+
+export function setNavigationNavbarPosition( obj ){
+	globalStore.setKey("navigation", { navbar: { position: obj } })
+}
+
+export function setNavigationNavbarIsMoving( bool ){
+	globalStore.setKey("navigation", { navbar: { isMoving: bool } })
+}
+
+
+// SEARCH
+export function setCurrentSearch( payload ){
+	globalStore.setKey("currentSearch", payload)
+}
