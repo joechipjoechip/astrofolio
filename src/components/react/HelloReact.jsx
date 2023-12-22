c6112947700fdc197dfc05b009b0695aab427372
Lionel <joechipjoechip@gmail.com>
2023-12-22 17:09:31 +0100

    add shader gradient to react component

--- src/components/react/HelloReact.jsx
+++ src/components/react/HelloReact.jsx
@@ -1,31 +1,58 @@
-import { StrictMode } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import "./HelloReact.css"
+import { ShaderGradientCanvas, ShaderGradient } from 'shadergradient'
+import * as reactSpring from '@react-spring/three'
+import * as drei from '@react-three/drei'
+import * as fiber from '@react-three/fiber'
 
-export default function Experience(){
-    return <>
+export default function HelloReact() {
 
-        <div className='react-canvas-wrapper'>
+    const params = {
+        brightness: 1.1,
+        cAzimuthAngle: 180,
+        cDistance: 3.9,
+        cPolarAngle: 115,
+        cameraZoom: 1,
+        color1: "5606FF",
+        color2: "FE8989",
+        color3: "000000",
+        destination: "onCanvas",
+        embedMode: "off",
+        envPreset: "city",
+        format: "gif",
+        fov: 45,
+        frameRate: 10,
+        grain: "off",
+        lightType: "3d",
+        pixelDensity: 1,
+        positionX: -0.5,
+        positionY: 0.1,
+        positionZ: 0,
+        range: "enabled",
+        rangeEnd: 40,
+        rangeStart: 0,
+        reflection: 0.1,
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 235,
+        shader: "defaults",
+        type: "waterPlane",
+        uAmplitude: 0,
+        uDensity: 2.1,
+        uFrequency: 5.5,
+        uSpeed: 0.1,
+        uStrength: 1.1,
+        uTime: 0.2,
+        wireframe: false,
+    }
 
-            <StrictMode>
-                
-                <Canvas>
-
-                    <OrbitControls makeDefault /> 
-
-                    <ambientLight args={["white", 0.5]} />
-
-                    <mesh rotation-x={Math.PI * -0.5} scale={10} position-y={-1} receiveShadow>
-                        <planeGeometry />
-                        <meshStandardMaterial color="limegreen" />
-                    </mesh>
-
-                </Canvas>
-
-            </StrictMode>
-
-        </div>
-
-    </>
+  return (
+    <ShaderGradientCanvas
+      importedfiber={{ ...fiber, ...drei, ...reactSpring }}
+      style={{
+        position: 'absolute',
+        top: 0,
+      }}
+    >
+      <ShaderGradient {...params} />
+    </ShaderGradientCanvas>
+  )
 }
