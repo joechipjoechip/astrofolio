4c58dd00d14ee01340abb3cd03ecd8551f611941
Lionel <joechipjoechip@gmail.com>
2023-12-23 11:39:54 +0100

    react component start to be interactive

--- src/components/react/HelloReact.jsx
+++ src/components/react/HelloReact.jsx
@@ -4,123 +4,122 @@ import * as drei from '@react-three/drei'
 import * as fiber from '@react-three/fiber'
 
 import "./HelloReact.css"
-import { useEffect, useRef } from "react"
+import { useEffect } from "react"
 
 import { useStore } from '@nanostores/react';
 import { globalStore } from "@/stores/globalStore.js"
 
-export default function HelloReact() {
+let changingDistance = 3.3
 
-    let changingValue = 1
-    const gradientWrapper = useRef()
+export default function HelloReact(){
+    
     const $store = useStore(globalStore)
 
-    console.log("store react : ", $store)
-
     useEffect(() => {
-        // console.log("from react store changed : ", $store.currentStepIndex)
-        if( $store.currentStepIndex === 1 ){
-            changingValue = 90
-        } else {
-            changingValue = 45
-        }
+        console.log("from react store changed : ", $store.currentStepIndex)
 
-        // if( gradientWrapper ){
-        //     console.log("gradientWrapper ref : ", gradientWrapper)
-        // }
+        if( $store.currentStepIndex === 1 ){
+            changingDistance = 3.9
+        } else {
+            changingDistance = 6.5
+        }
 
     }, [$store.currentStepIndex])
 
-    // const params = {
-    //     orbitControls: "disabled",
-
-    //     brightness: 1.1,
-    //     cAzimuthAngle: 180,
-    //     cDistance: 3.9,
-    //     cPolarAngle: 115,
-    //     cameraZoom: 1,
-    //     color1: "5606FF",
-    //     color2: "FE8989",
-    //     color3: "000000",
-    //     destination: "onCanvas",
-    //     embedMode: "off",
-    //     envPreset: "city",
-    //     format: "gif",
-    //     // fov: 45,
-    //     frameRate: 10,
-    //     grain: "off",
-    //     lightType: "3d",
-    //     pixelDensity: 1,
-    //     positionX: -0.5,
-    //     positionY: 0.1,
-    //     positionZ: 0,
-    //     range: "enabled",
-    //     rangeEnd: 40,
-    //     rangeStart: 0,
-    //     reflection: 0.1,
-    //     rotationX: 0,
-    //     rotationY: 0,
-    //     rotationZ: 235,
-    //     shader: "defaults",
-    //     type: "waterPlane",
-    //     uAmplitude: 0,
-    //     uDensity: 2.1,
-    //     uFrequency: 5.5,
-    //     uSpeed: 0.1,
-    //     uStrength: 1.1,
-    //     uTime: 0.2,
-    //     wireframe: false,
-    // }
+    // fiber.useFrame((state, thing) => {
+    //     console.log("test useFrame : ", state)
+    // })
 
     const params = {
-        animate: "on",
-        axesHelper: "off",
-        bgColor1: "#000000",
-        bgColor2: "#000000",
-        brightness: "1.1",
-        cAzimuthAngle: "180",
-        cDistance: "3.3",
-        cPolarAngle: "90",
-        cameraZoom: "9.1",
-        color1: "ff52b4",
-        color2: "ffbd96",
-        color3: "f6eddb",
+        orbitControls: "disabled",
+
+        brightness: 1.1,
+        cAzimuthAngle: 180,
+        // cDistance: 3.9,
+        cPolarAngle: 115,
+        cameraZoom: 1,
+        color1: "5606FF",
+        color2: "FE8989",
+        color3: "000000",
         destination: "onCanvas",
         embedMode: "off",
-        // envPreset: "city",
+        envPreset: "city",
         format: "gif",
-        fov: "20",
-        frameRate: "10",
-        gizmoHelper: "hide",
+        // fov: 45,
+        frameRate: 10,
         grain: "off",
         lightType: "3d",
-        pixelDensity: "1",
-        positionX: "0",
-        positionY: "0.7",
-        positionZ: "0",
-        range: "disabled",
-        rangeEnd: "40",
-        rangeStart: "0",
-        reflection: "0.1",
-        rotationX: "50",
-        rotationY: "0",
-        rotationZ: "0",
+        pixelDensity: 1,
+        positionX: -0.5,
+        positionY: 0.1,
+        positionZ: 0,
+        range: "enabled",
+        rangeEnd: 40,
+        rangeStart: 0,
+        reflection: 0.1,
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 235,
         shader: "defaults",
         type: "waterPlane",
-        uAmplitude: "0",
-        uDensity: "2.5",
-        uFrequency: "0",
-        uSpeed: "0.1",
-        uStrength: "1",
-        uTime: "8",
-        wireframe: "false",
+        uAmplitude: 0,
+        uDensity: 2.1,
+        uFrequency: 5.5,
+        uSpeed: 0.1,
+        uStrength: 1.1,
+        uTime: 0.2,
+        wireframe: false,
     }
+
+    // const params = {
+    //     animate: "on",
+    //     axesHelper: "off",
+    //     bgColor1: "#000000",
+    //     bgColor2: "#000000",
+    //     brightness: "1.2",
+    //     cAzimuthAngle: "180",
+    //     // cDistance: changingDistance,
+    //     cPolarAngle: "90",
+    //     cameraZoom: "9.1",
+    //     color1: "ff52b4",
+    //     color2: "ffbd96",
+    //     color3: "f6eddb",
+    //     destination: "onCanvas",
+    //     embedMode: "off",
+    //     // envPreset: "city",
+    //     format: "gif",
+    //     fov: "20",
+    //     frameRate: "10",
+    //     gizmoHelper: "hide",
+    //     grain: "off",
+    //     lightType: "3d",
+    //     pixelDensity: "1",
+    //     positionX: "0",
+    //     positionY: "0.7",
+    //     positionZ: "0",
+    //     range: "disabled",
+    //     rangeEnd: "40",
+    //     rangeStart: "0",
+    //     reflection: "0.1",
+    //     rotationX: "50",
+    //     rotationY: "0",
+    //     rotationZ: "0",
+    //     shader: "defaults",
+    //     type: "waterPlane",
+    //     uAmplitude: "0",
+    //     uDensity: "2.5",
+    //     uFrequency: "0",
+    //     uSpeed: "0.1",
+    //     uStrength: "1",
+    //     uTime: "8",
+    //     wireframe: "false",
+    // }
 
   return (
     <>
         <div className='react-canvas-wrapper'>
             <ShaderGradientCanvas
-                ref={ gradientWrapper }
+                // ref={ gradientWrapper }
                 importedfiber={{ ...fiber, ...drei, ...reactSpring }}
                 style={{
                     position: 'absolute',
@@ -128,8 +127,8 @@ export default function HelloReact() {
                 }}
             >
                 <ShaderGradient 
-                    {...params} 
-                    fov={changingValue}
+                    {...params}
+                    cDistance={changingDistance}
                 />
             </ShaderGradientCanvas>
         </div>
