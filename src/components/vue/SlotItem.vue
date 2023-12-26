25bc1a2b9cc3543d59b9067aa040662279e170cb
Lionel <joechipjoechip@gmail.com>
2023-12-27 00:25:19 +0100

    remove default value for scrollintoview

--- src/components/vue/SlotItem.vue
+++ src/components/vue/SlotItem.vue
@@ -85,7 +85,7 @@ const textsWrapper = ref(null)
 const technosWrapper = ref(null)
 
 function scrollSlotAtTop(){
-	slotWrapperElement.value.scrollIntoView({inline: "start", behavior: "smooth"});
+	slotWrapperElement.value.scrollIntoView({behavior: "smooth"});
 }
 
 // // - - - - - - - - - - - - - 
