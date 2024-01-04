a1fde61cb7223b2c9ed6469bfbd805ea4513f66a
Lionel <joechipjoechip@gmail.com>
2024-01-04 19:22:33 +0100

    svelte : basics for menu

--- src/components/svelte/Navigation.svelte
+++ src/components/svelte/Navigation.svelte
@@ -1,5 +1,7 @@
 
 <script>
+    import IconMain from "./icons/uiMenu.svelte"
+
     import { 
         globalStore, 
         setCurrentStepIndex,
@@ -7,37 +9,113 @@
     } from "@/stores/globalStore.js"
 
     const message = "hello, imma wip svelte component"
+    let menusAreOpen = false
+
+    function handleMainMouseEnter(){
+        console.log("mouse enter menu")
+        menusAreOpen = true
+    }
+    
+    function handleSubMouseLeave(){
+        menusAreOpen = false
+    }
 
 </script>
 
 <nav class="nav-wrapper">
+    
     <!-- <h2>store data - {globalStore.value.name}</h2> -->
-    <h2>message : {message}</h2>
-    <button 
+    <!-- <h2>message : {message}</h2>
+        <button 
         class="nav-item"
         on:click={() => setCurrentStepIndex(1) }
-    >
+        >
         go formation
     </button>
-
+    
     <button 
-        class="nav-item"
-        on:click={() => window.toggleColorMode() }
+    class="nav-item"
+    on:click={() => window.toggleColorMode() }
     >
-        switch dark/light
-    </button>
+    switch dark/light
+</button> -->
+
+    <div class="nav-inner">
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div 
+            class="nav-main"
+            on:mouseenter={() => handleMainMouseEnter()}
+        >
+            <svelte:component 
+                this={IconMain} 
+                width={'2.5rem'}
+                height={'2.5rem'}
+            />
+        </div>
+    
+        <!-- HORIZONTAL -->
+        {#if menusAreOpen}
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div 
+            class="nav-sub-horizontal"
+            on:mouseleave={() => handleSubMouseLeave()}
+        >
+            <p>horizontal</p>
+        </div>
+        {/if}
+
+        <!-- VERTICAL -->
+        {#if menusAreOpen}
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div 
+            class="nav-sub-vertical"
+            on:mouseleave={() => handleSubMouseLeave()}
+        >
+            <p>vertical</p>
+        </div>
+        {/if}
+
+    </div>
 </nav>
 
 <style lang="scss">
+    $subEdgeFromMain: 6rem;
     .nav {
         &-wrapper {
             z-index: 999; 
             position: absolute;
-            right: 0;
+            top: 2rem;
+            right: 2rem;
         }
 
-        &-item {
-            border: solid 5px orange;
+        &-inner {
+            position: relative;
+            border: solid 1px red;
+        }
+
+        &-main {
+            padding: 0.75rem;
+            background-color: var(--color-contrast-25);
+            border-radius: 50%;
+        }
+
+        &-sub {
+            &-horizontal {
+                position: absolute;
+                right: $subEdgeFromMain;
+                top: 0;
+                height: 100%;
+            }
+            &-vertical {
+                position: absolute;
+                top: $subEdgeFromMain;
+                width: 100%;
+            }
+
+            &-horizontal,
+            &-vertical {
+                border: solid 1px lime;
+            }
         }
     }
 </style>
