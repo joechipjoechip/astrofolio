4ae2fb41217d2d6efc1aba7991794cfa467d001f
Lionel <joechipjoechip@gmail.com>
2024-01-04 16:55:20 +0100

    tuning wip nav

--- src/components/svelte/Navigation.svelte
+++ src/components/svelte/Navigation.svelte
@@ -6,13 +6,13 @@
         setColorMode,
     } from "@/stores/globalStore.js"
 
-    const message = "hello there, imma svelte component"
+    const message = "hello, imma wip svelte component"
 
 </script>
 
 <nav class="nav-wrapper">
     <!-- <h2>store data - {globalStore.value.name}</h2> -->
-    <h2>message -> {message}</h2>
+    <h2>message : {message}</h2>
     <button 
         class="nav-item"
         on:click={() => setCurrentStepIndex(1) }
